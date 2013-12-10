dfce16c424150cd4862f4d59d8e39a8ea1d9c471
cnryb <cnryb@live.cn>
2013-12-10 16:38:24 +0800

    最开始的版本

---
+++ RegexTester/Main.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace RegexTester
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtReg = new System.Windows.Forms.TextBox();
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.lb = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtReg
+            // 
+            this.txtReg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtReg.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtReg.Location = new System.Drawing.Point(3, 2);
+            this.txtReg.Multiline = true;
+            this.txtReg.Name = "txtReg";
+            this.txtReg.Size = new System.Drawing.Size(186, 109);
+            this.txtReg.TabIndex = 0;
+            this.txtReg.TextChanged += new System.EventHandler(this.txtReg_TextChanged);
+            // 
+            // rtb
+            // 
+            this.rtb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtb.Location = new System.Drawing.Point(195, 2);
+            this.rtb.Name = "rtb";
+            this.rtb.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.rtb.Size = new System.Drawing.Size(321, 387);
+            this.rtb.TabIndex = 2;
+            this.rtb.Text = "";
+            this.rtb.Leave += new System.EventHandler(this.rtb_Leave);
+            // 
+            // lb
+            // 
+            this.lb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lb.FormattingEnabled = true;
+            this.lb.ItemHeight = 12;
+            this.lb.Location = new System.Drawing.Point(3, 118);
+            this.lb.Name = "lb";
+            this.lb.Size = new System.Drawing.Size(186, 268);
+            this.lb.TabIndex = 3;
+            this.lb.DoubleClick += new System.EventHandler(this.lb_DoubleClick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 393);
+            this.Controls.Add(this.lb);
+            this.Controls.Add(this.rtb);
+            this.Controls.Add(this.txtReg);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtReg;
+        private System.Windows.Forms.RichTextBox rtb;
+        private System.Windows.Forms.ListBox lb;
+    }
+}
---
+++ RegexTester/Main.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace RegexTester
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+
+        public void Check(string strReg)
+        {
+            if (string.IsNullOrEmpty(strReg) || string.IsNullOrEmpty(rtb.Text)) return;
+
+            // ChangeRichTextBox(rtb, 0, rtb.Text.Length, Color.White, Color.Black);
+
+            try
+            {
+                DataTable dt = new DataTable();
+                dt.Columns.Add("DisplayMember");
+                dt.Columns.Add("ValueMember");
+                var reg = new Regex(strReg);
+                int startindex = 0;
+                foreach (Match item in reg.Matches(rtb.Text))
+                {
+                    var res = item.Value;
+                    int startLocation = rtb.Text.IndexOf(res, startindex);
+                    startindex = startLocation + res.Length;
+                    DataRow dr = dt.NewRow();
+                    dr["DisplayMember"] = res;
+                    dr["ValueMember"] = startLocation + "#" + res.Length;
+                    dt.Rows.Add(dr);
+
+                    ChangeRichTextBox(rtb, startLocation, res.Length, Color.Yellow, Color.Red);
+                }
+
+                lb.DataSource = dt;
+                lb.DisplayMember = "DisplayMember";
+                lb.ValueMember = "ValueMember";
+            }
+            catch (Exception ex)
+            {
+                Text = "正则表达式异常 " + ex;
+            }
+
+        }
+
+        private void ChangeRichTextBox(RichTextBox rt, int start, int length, Color bgc, Color c)
+        {
+            int currentNum = rtb.SelectionStart;
+            rt.Select(start, length);
+
+            rtb.SelectionStart = currentNum;
+            rt.SelectionColor = c;
+            rt.SelectionBackColor = bgc;
+        }
+        private void txtReg_TextChanged(object sender, EventArgs e)
+        {
+            Text = "";
+            Check(txtReg.Text);
+        }
+
+        private void lb_DoubleClick(object sender, EventArgs e)
+        {
+            DataTable dt = lb.DataSource as DataTable;
+            if (dt != null)
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    string[] strs = dt.Rows[i]["ValueMember"].ToString().Split('#');
+                    if (strs.Length != 2) continue;
+                    int start, length;
+                    int.TryParse(strs[0], out start);
+                    int.TryParse(strs[1], out length);
+                    rtb.SelectionStart = start;
+                    rtb.Select(start, length);
+                    rtb.SelectionBackColor = Color.Yellow;
+                }
+            {
+                if (lb.SelectedValue == null) return;
+                string[] strs = lb.SelectedValue.ToString().Split('#');
+                if (strs.Length != 2) return;
+                int start, length;
+                int.TryParse(strs[0], out start);
+                int.TryParse(strs[1], out length);
+                rtb.SelectionStart = start;
+                rtb.Select(start, length);
+                rtb.SelectionBackColor = Color.Blue;
+                rtb.ScrollToCaret();
+            }
+        }
+
+        private void rtb_Leave(object sender, EventArgs e)
+        {
+            //rtb.ScrollToCaret();
+            //int currentColumnNumber = rtb.SelectionStart - rtb.GetFirstCharIndexOfCurrentLine()+1;//光标所在本行的位置
+            //int lineNum = 1 + rtb.GetLineFromCharIndex(rtb.SelectionStart);//光标所在的行
+            // int currentNum = rtb.SelectionStart;
+            Check(txtReg.Text);
+            //rtb.SelectionStart = currentNum;
+            //rtb.SelectionLength = 0;
+        }
+    }
+}
